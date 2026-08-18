a801a362f0ce0cc14f8346822a7d74322bdfa0ea
Sean Murphy <sean@iamseanmurphy.com>
2026-08-18 23:34:45 +0000

    4.0.2

--- dhl-for-woocommerce/vendor/composer/installed.php
+++ dhl-for-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'pr/dhl-logistic-services-woocommerce',
-        'pretty_version' => '401.x-dev',
-        'version' => '401.9999999.9999999.9999999-dev',
-        'reference' => 'f3bc7e554cff3b9366999dd263b1b032fbc02fef',
+        'pretty_version' => '402.x-dev',
+        'version' => '402.9999999.9999999.9999999-dev',
+        'reference' => '051c16fae4eeb22387f5576692c65e9cf9712e04',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'pr/dhl-logistic-services-woocommerce' => array(
-            'pretty_version' => '401.x-dev',
-            'version' => '401.9999999.9999999.9999999-dev',
-            'reference' => 'f3bc7e554cff3b9366999dd263b1b032fbc02fef',
+            'pretty_version' => '402.x-dev',
+            'version' => '402.9999999.9999999.9999999-dev',
+            'reference' => '051c16fae4eeb22387f5576692c65e9cf9712e04',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
